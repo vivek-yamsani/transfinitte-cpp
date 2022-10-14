e48f157e60a50183062b215922dcf2dd4517c374
vivek <vivekyamsani@gmail.com>
2022-10-15 01:57:55 +0530

    feat: auth

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require("express")
-const cors = require("cors")
-require('dotenv').config({ path: './env' })
+const cors = require("cors");
+require('dotenv').config({ path: './.env' })
 const app = express();
 
-const port = process.env.PORT ? process.env.PORT : 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+const port = process.env.PORT || 3000;
+
+const { authRouter } = require("./src/routers/auth");
+
+app.use("/auth", authRouter);
 
 app.all('*', async (req, res) => {
     res.status(404).json({
