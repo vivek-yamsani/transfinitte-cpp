60390268c72fb81fb4708a4891f29e26b70d2fc4
vivek <vivekyamsani@gmail.com>
2022-10-15 12:59:59 +0530

    refactor: init.js

--- backend/src/helpers/init.js
+++ backend/src/helpers/init.js
@@ -1,50 +1,41 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
+const prisma = require("./prisma")
+const bcrypt = require("bcrypt")
 async function main() {
-
-  const hashedPasswd = await bcrypt.hash( 'admin' , salt) 
   
+  const salt = await bcrypt.genSalt();
+  const hashedPasswd = await bcrypt.hash('admin', salt)
+
   const user = await prisma.user.create(
     {
       data: {
-        id: "admin" ,
+        id: "admin",
         name: 'admin',
         phone: '0000000000',
         role: 'ADMIN',
         email: 'admin@nitt.edu',
-        password: hashedPasswd ,
+        password: hashedPasswd,
 
       },
     }
   )
-  console.log(user) 
+  console.log(user)
   const department = await prisma.department.createMany({
     data: [
-  	{ id: 02  , name: 'CHEM'  },
-	{ id: 03  , name: 'CIVIL'  },
-	{ id: 06  , name: 'CSE'  },
-	{ id: 07  , name: 'EEE'  },
-	{ id: 08  , name: 'ECE'  },
-	{ id: 10  , name: 'ICE'  },
-	{ id: 11  , name: 'MECH'  },
-	{ id: 12  , name: 'MME'  },
-	{ id: 14  , name: 'PROD'  },
-	{ id: 01  , name: 'ARCH'  },
+      { id: 02, name: 'CHEM' },
+      { id: 03, name: 'CIVIL' },
+      { id: 06, name: 'CSE' },
+      { id: 07, name: 'EEE' },
+      { id: 08, name: 'ECE' },
+      { id: 10, name: 'ICE' },
+      { id: 11, name: 'MECH' },
+      { id: 12, name: 'MME' },
+      { id: 14, name: 'PROD' },
+      { id: 01, name: 'ARCH' },
     ],
     skipDuplicates: true,
- })
+  })
 
   console.log(department)
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
