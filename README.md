96ebab80c532460234e862f4ea49c4a0c556c720
unknown <velmavikas5123@gmail.com>
2022-10-16 11:03:18 +0530

    docs : readme

---
+++ README.md
@@ -0,0 +1,32 @@
+# Campus Placement Portal
+
+This is the repo for the Campus Placement Portal project which is a part of Transfinitte Hackathon
+
+### Requirements:
+- Node JS
+- MySQL
+
+### Technologies and packages used:
+- frontend
+    - react JS
+    - chakra-ui lib
+- backend
+    - express
+    - prisma (ORM)
+
+## Getting Started
+
+- add the env variables in backend app according to .env.example
+
+- Installing dependencies
+
+```bash
+cd backend && npm i
+cd frontend && npm i
+```
+
+- Start servers
+```bash
+cd backend && npm run dev
+cd frontend && npm start
+```
--- frontend/ccp/src/screens/details.js
+++ frontend/ccp/src/screens/details.js
@@ -1,30 +1,44 @@
-import { Button, Heading, VStack, Text } from "@chakra-ui/react"
-import { useEffect, useState } from "react"
+import { Button, Heading, VStack, Text, Textarea } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { API_URL } from "../constants";
 import { Fetch } from "../fetch_function";
 
 export default function Details({ id, name }) {
-    const [details, setDetails] = useState({});
-    const [userRole, setRole] = useState("");
-    useEffect(async () => {
-        const temp1 = await Fetch(`${API_URL}/announcements/${id}`, null, 'GET');
-        const temp2 = await Fetch(`${API_URL}/roleFind`);
-        setDetails(temp1);
-        setRole(temp2);
-    }, [])
-    return (<>
-        {userRole == "" ? <></> : (
+  const [details, setDetails] = useState({});
+  const [userRole, setRole] = useState("");
+  useEffect(async () => {
+    const temp1 = await Fetch(`${API_URL}/announcements/${id}`, null, "GET");
+    const temp2 = await Fetch(`${API_URL}/roleFind`);
+    setDetails(temp1);
+    setRole(temp2);
+  }, []);
+  return (
+    <>
+      {userRole == "" ? (
+        <></>
+      ) : (
+        <>
+          {userRole == "STUDENT" ? (
             <>
-                {userRole == "STUDENT" ? (
-                    <>
-                        <VStack>
-                            <Heading>{details.name}</Heading>
-                        </VStack>
-                    </>
-                ) : (<></>)}
+              <VStack>
+                <Text>Title</Text>
+                <Heading>{details.title}</Heading>
+                <Text>Description</Text>
+                <Text>{details.description}</Text>
+              </VStack>
             </>
-        )}
+          ) : (
+            <>
+              <VStack>
+                <Text>Title</Text>
+                <Heading>{details.title}</Heading>
+                <Text>Description</Text>
+                <Text>{details.description}</Text>
+              </VStack>
+            </>
+          )}
+        </>
+      )}
     </>
-    )
-
+  );
 }
